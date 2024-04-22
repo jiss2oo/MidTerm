90e330d6cb2f7ab81e7e88808bb321558fbc79c2
Hyerim22 <jhl8583@naver.com>
2024-04-22 16:53:53 +0900

    시험제출
    교수님... 저는 행복해욤 ദ്ദി  ͡° ͜ʖ ͡°)

--- Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -25,9 +25,11 @@ public class PlayerMovement : MonoBehaviour
     private Quaternion rotation = Quaternion.identity;
 
     private void Start()
-    {        
+    {
         // 문제 1) 위에 주어진 animator, rb 변수를 이용해 Component를 받아오는 명령어를 작성하세요.
         // (Component당 5점, 합계 5*2=10점, 부분점수 있음)
+        float Animator = animator;
+        float Rigidbody = rb;
 
     }
 
@@ -37,6 +39,9 @@ public class PlayerMovement : MonoBehaviour
         // 0f 대신 정답을 채워넣으면 작동합니다. (각 변수당 5점, 총 10점)
         float horizontal = 0f; // 이 변수와
         float vertical = 0f; // 이 변수를 사용해야 합니다.
+
+        bool horizontal = !Mathf.Approximately(movement.x, 0f);
+        bool vertical = !Mathf.AreApproximately(movement.y, 0f);
         
         #region 건드리면 작동 안 됨
         movement.Set(horizontal, 0f, vertical);
@@ -45,17 +50,29 @@ public class PlayerMovement : MonoBehaviour
 
         // 문제 3) isWalking은 hasHorizontalInput과 hasVerticalInput중 하나만 입력되어도 true(참)가 되어야 합니다.
         // false를 지우고 제대로 된 명령줄을 완성해 주세요. (10점)
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);        
 
-        bool isWalking = false;
+        //bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        //bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);     
+        
+        bool isWalking = !Mathf.IsWalking(movement.x, 0f);
+
+        //bool isWalking = false;
 
         // 문제 4) isWalking 변수를 Animator의 IsWalking 파라미터에 적용하세요. (10점)
 
+        animator.IsWalking(isWalking);
 
         // 문제 5) 원하는 방향으로 이동할 수 있는 Vector3값을 만드는 명령줄을 Vector3.zero를 지우고 완성해 주세요. (10점)
-        Vector3 desiredFoward = Vector3.zero;
+       // Vector3 desiredFoward = Vector3.zero;
+
+        Vector3 desiredFoward = vertical *
         rotation = Quaternion.LookRotation(desiredFoward);
+
+        //교수님 너무 어려워요힝힝힝힝 ㅠ ㅠ
+        //자동완성........으로 해결 몬해요..... 첨부터 끝까지 다 문ㄷ제인 거 가타욤
+        //교수님 제 점수는 매우 하찮겠지만 전 교수님 수업 최고 조와합니다.........
+        //점수는 점수일 뿐 저는 정말 즐겁게 수업을 들었습니다.....
+        //교수님 최공....
 
     }
 
